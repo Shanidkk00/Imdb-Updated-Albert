91ec7850b837ca71ff1587e98a03e0256ee8bf44
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-02-13 22:06:19 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Sreejithmadmax/DonLee-Robot-V2.git /DonLee-Robot-V2
+  git clone https://github.com/Sreejithmadmax/Imdb-Updated-Albert.git /Imdb-Updated-Albert
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DonLee-Robot-V2
+  git clone $UPSTREAM_REPO /Imdb-Updated-Albert
 fi
-cd /DonLee-Robot-V2
+cd /Imdb-Updated-Albert
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
